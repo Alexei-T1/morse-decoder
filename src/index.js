b4bb0988dc490a354cebaefd3c3cc149c2e37d90
Alexei-T1 <turencko.alexei@yandex.ru>
2021-08-04 15:48:35 +0400

    feat: add code for solution this task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    expr = expr.split('**********');
+    let resultArr = [];
+
+label: for (let str of expr) {
+        let tempArr = [];
+        for (let i = 0; i < str.length; i += 2) {
+            let tempS = ''
+            if (str[i] + str[i + 1] == '00') {
+                    continue;
+            }
+            else if (str[i] + str[i + 1] == '10') {
+                    tempS = '.';
+            }
+            else if (str[i] + str[i + 1] == '11') {
+                    tempS = '-';
+            }
+            tempArr.push(tempS);
+
+            if ( i != 0 && (i + 2) % 10 == 0) {
+                resultArr.push([tempArr.join("")]);
+                tempArr = [];
+            }
+        }
+        resultArr.push(' ');
+    }
+
+    resultArr = resultArr.map(w => {
+        if (w == ' ') return ' ';
+        else {
+        return MORSE_TABLE[w];
+        }
+
+    }).join('').trim();
+
+
+    return resultArr;
 }
 
 module.exports = {
